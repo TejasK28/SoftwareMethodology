a8b49c9b4b7dd4c03ba14fec3068799c5ca6901d
Tejas <87278292+TejasK28@users.noreply.github.com>
2024-02-03 23:47:38 +0000

    Problem Set 2

--- ModifiersPackage/Modifiers.java
+++ ModifiersPackage/Modifiers.java
@@ -1,6 +1,15 @@
 package ModifiersPackage;
+import ModifiersPackage.abcpackage.*;
 
 public class Modifiers 
 {
-    
+    public static void main(String[] args) {
+        
+        A a = new A();
+        /*
+         * You can't access a protected field via an instance
+         */
+        //System.out.println(a.protectedInt);
+    }
+
 }
---
+++ PSET2/Interface.java
@@ -0,0 +1,104 @@
+package PSET2;
+
+/*
+ * Problem 1:
+ *      C class implements comparable class of type D
+ * 
+ * this succesfully compiles!
+ */
+class D {}
+
+class C implements Comparable<D>
+{
+
+    @Override
+    public int compareTo(D o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    }
+
+}
+
+/*
+ * Problem 2:
+ * 
+ * This won't compile because the a class can't implement the same
+ * interface twice with different arguments
+ * 
+ * had to comment out the C2 clas
+ */
+
+class D2 {}
+
+// class C2 implements Comparable<C2>, Comparable<D2>
+// {
+
+// } 
+
+
+/*
+ * Problem 3:
+ * 
+ * Wouldn't compile for the same reason as Problem 2
+ */
+
+ class A implements Comparable<A>
+ {
+    public int compareTo(A a) {return 0;}
+ }
+
+//  class B extends A implements Comparable<B>
+//  {
+
+//  }
+
+
+/*
+ * Problem 4:
+ * 
+ * This obviously failed because we never implemented the 
+ * method
+ */
+
+interface I {void stuff();}
+interface J {void stuff();}
+
+// class F implements I,J {
+
+// }
+
+
+/*
+ * Problem 5:
+ * 
+ * This works because the compiler cares
+ * only if the requirement is satisfied
+ */
+
+ interface I2 {void stuff();}
+interface J2 {void stuff();}
+
+class F2 implements I2,J2 {
+
+    public void stuff() {
+        System.out.println("Sccessfully implemented");
+    }}
+
+/*
+ * Problem 6:
+ */
+
+
+ /*
+  * Problem 7:
+  */
+
+  
+
+
+public class Interface 
+{
+    public static void main(String[] args) {
+        System.out.println("SUCCESS!");
+    }
+}
